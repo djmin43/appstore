bc9c4f9af1bd6ea4adbf21ecc42f89c6587e2ce5
jay <dj.min43@gmail.com>
2022-08-19 22:40:07 +0900

    add option

--- international-website/gatsby-config.ts
+++ international-website/gatsby-config.ts
@@ -26,6 +26,7 @@ const config: GatsbyConfig = {
       options: {
         langKeyDefault: "en",
         useLangKeyLayout: false,
+        prefixDefault: true,
       },
     },
     "gatsby-plugin-image",
