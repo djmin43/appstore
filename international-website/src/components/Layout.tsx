e2d0256eee161ef46cd93a3241b71aaef635c54c
jay <dj.min43@gmail.com>
2022-08-20 19:47:56 +0900

    get i18next config

--- international-website/src/components/Layout.tsx
+++ international-website/src/components/Layout.tsx
@@ -1,23 +1,12 @@
 import React from "react";
-import useRedirectLanguage from "../hooks/useRedirectLanguage";
-import { graphql } from "gatsby";
+import "../i18n/config";
 
 interface ILayout {
   children: JSX.Element;
 }
 
 const Layout = ({ children }: ILayout) => {
-  // useRedirectLanguage();
-
   return <div>{children}</div>;
 };
-
-export const data = graphql`
-  query MyQuery {
-    contentfulHarmonyMain {
-      node_locale
-    }
-  }
-`;
 
 export default Layout;
--- international-website/src/pages/index.tsx
+++ international-website/src/pages/index.tsx
@@ -1,41 +1,15 @@
 import React from "react";
-import { Link, useI18next } from "gatsby-plugin-react-i18next";
 import Layout from "../components/Layout";
-import { graphql } from "gatsby";
+import { useTranslation } from "react-i18next";
 
 const Index = () => {
-  const { languages, changeLanguage, originalPath } = useI18next();
-  console.log(navigator.language);
-  console.log(originalPath);
-  console.log(languages);
-
+  const { t } = useTranslation();
+  console.log(t);
   return (
     <Layout>
-      <ul className="languages">
-        {languages.map((lng) => (
-          <li key={lng}>
-            <Link to={originalPath} language={lng}>
-              {lng}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div>hello</div>
     </Layout>
   );
 };
-
-export const query = graphql`
-  query LanguageQuery {
-    allLocale {
-      edges {
-        node {
-          id
-          ns
-          language
-        }
-      }
-    }
-  }
-`;
 
 export default Index;
